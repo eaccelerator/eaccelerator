0791908264ad8c936e4a64846bd3064cb8603977
Bart Vanbrabant <bart@vanbrabant.eu>
2005-05-06 08:26:43 +0000

    User who supplied patch seemed to use the version with oloader. Fixing again.

--- encoder.php
+++ encoder.php
@@ -60,7 +60,7 @@ function eaccelerator_encode_file($src, $out, $f, $c, $w, $web) {
     }
   } else {
     if (!$w) {
-        $cmp = $prefix.'<?php if (!is_callable("eaccelerator_load") && !@dl((PHP_OS=="WINNT"||PHP_OS=="WIN32")?"oloader.dll":"oloader.so")) { die("This PHP script has been encoded with eAccelerator, to run it you must install <a href=\"http://eaccelerator.sourceforge.net/\">eAccelerator or eLoader</a>");} return eaccelerator_load(\''.$cmp."');?>\n";
+        $cmp = $prefix.'<?php if (!is_callable("eaccelerator_load") && !@dl((PHP_OS=="WINNT"||PHP_OS=="WIN32")?"eloader.dll":"eloader.so")) { die("This PHP script has been encoded with eAccelerator, to run it you must install <a href=\"http://eaccelerator.sourceforge.net/\">eAccelerator or eLoader</a>");} return eaccelerator_load(\''.$cmp."');?>\n";
     }
     else
     {
