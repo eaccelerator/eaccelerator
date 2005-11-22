770a225b8b05d55194f8df261f273014ba4a523d
Bart Vanbrabant <bart@vanbrabant.eu>
2005-11-22 20:59:12 +0000

    Some compile fixes

--- eaccelerator.c
+++ eaccelerator.c
@@ -1126,9 +1126,10 @@ static int eaccelerator_stat(zend_file_handle *file_handle,
     }
 
 	if (zend_is_executing(TSRMLS_C)) {
+        int tryname_length;
 		strncpy(tryname, zend_get_executed_filename(TSRMLS_C), MAXPATHLEN);
 		tryname[MAXPATHLEN - 1] = 0;
-		int tryname_length = strlen(tryname);
+		tryname_length = strlen(tryname);
 
 		while (tryname_length >= 0 && !IS_SLASH(tryname[tryname_length]))
 			tryname_length--;
@@ -1251,12 +1252,12 @@ ZEND_DLEXPORT zend_op_array* eaccelerator_compile_file(zend_file_handle *file_ha
   int   nreloads;
   time_t compile_time;
   int stat_result = 0;
+  struct timeval tv_start;
 
 #ifdef EACCELERATOR_USE_INODE
   realname[0] = '\000';
 #endif
 
-  struct timeval tv_start;
   ea_debug_printf(EA_TEST_PERFORMANCE, "[%d] Enter COMPILE\n",getpid());
   ea_debug_start_time(&tv_start);
   ea_debug_printf(EA_DEBUG, "[%d] Enter COMPILE\n",getpid());
