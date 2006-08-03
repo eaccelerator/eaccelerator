dd7f2cb2034e0a78bd2683a787388de6407b54e2
Bart Vanbrabant <bart@vanbrabant.eu>
2006-08-03 11:24:22 +0000

    * zend_do_inheritance can't be called with TSRMLS_CC when using   php 4, this also means that eAccelerator won't work with threading! * ea_info and ea_dasm: if eaccelerator_mm_instance is NULL, just return   null and don't do something.

--- ea_restore.c
+++ ea_restore.c
@@ -873,7 +873,11 @@ static zend_class_entry *restore_class_entry(zend_class_entry * to, ea_class_ent
 	restore_class_methods(to TSRMLS_CC);
 #endif
 	if (to->parent)
+#ifdef ZEND_ENGINE_2
 		zend_do_inheritance(to, to->parent TSRMLS_CC);
+#else
+        zend_do_inheritance(to, to->parent);
+#endif
 	EAG(class_entry) = old;
 
 #ifdef DEBUG
