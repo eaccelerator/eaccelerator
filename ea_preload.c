2d9e9e7cb1a07c3944a38ff9cd4b6d4734a48e02
woooha <woooha@gmail.com>
2013-03-04 13:35:19 +0800

    Fixed the bug that if ea_mm_instance is NULL, eaccelerator_compile_file will segement fault 11.

--- ea_preload.c
+++ ea_preload.c
@@ -1,4 +1,7 @@
 #include "ea_preload.h"
+#define MAX_EACC_FILENAME_LEN 65535
+
+extern eaccelerator_mm * ea_mm_instance;
 
 PHP_FUNCTION(eaccelerator_preload_file){
     char * filename;
@@ -9,12 +12,15 @@ PHP_FUNCTION(eaccelerator_preload_file){
         RETURN_FALSE;
     }
 
-    filename[filename_len] = 0;
-
     if( FAILURE == zend_stream_open(filename, &file_handle TSRMLS_CC) ){
         RETURN_FALSE;
     }
-    if( NULL == eaccelerator_compile_file(&file_handle, ZEND_REQUIRE TSRMLS_CC) ){
+
+    if( NULL == ea_mm_instance ){
+        RETURN_FALSE;
+    }
+
+    if( !eaccelerator_compile_file(&file_handle, ZEND_REQUIRE TSRMLS_CC) ){
         RETURN_FALSE;
     }
 
